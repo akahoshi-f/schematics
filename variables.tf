c93dabf7bce53c80c8daa077463167c59103dbd1
Felipe Akahoshi <felipeakahoshide@gmail.com>
2025-11-06 14:11:00 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "ibmcloud_api_key_p1nonpci" {}
+variable "proj_name" {}
+variable "region" {}
+variable "zone" {}
+variable "rg" {}
+
+######################################################################
+
+######################################################################
+# var lpar
+######################################################################
+variable "ssh" {}
+variable "subnet_cidr" {}
+variable "pi_memory" {}
+variable "pi_processors" {}
+variable "pi_proc_type" {}
+variable "pi_image_id" {}
+variable "pi_sys_type" {}
+variable "pi_pin_policy" {}
+variable "pi_ip_address" {}
+
+######################################################################
+
+######################################################################
+# outputs externos
+######################################################################
+variable "tg_nonpci_id" {}
+variable "pvs_connection" {}
+
+######################################################################
